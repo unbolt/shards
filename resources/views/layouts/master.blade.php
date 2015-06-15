45d1fca864b4054e92ebd1f7f532f49cff4baa58
unbolt <dan@unbo.lt>
2015-06-15 14:27:37 +0100

    Adding initial master template

---
+++ resources/views/layouts/master.blade.php
@@ -0,0 +1,18 @@
+<!DOCTYPE html>
+<html>
+    <head>
+        <title>Shards - @yield('title')</title>
+
+        <!-- include some styles -->
+        <link href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.4/css/bootstrap.min.css" rel="stylesheet">
+        <link href="/css/shards.css" rel="stylesheet">
+
+        <!-- include our scripts -->
+        <script src="//code.jquery.com/jquery-1.11.3.min.js"></script>
+        <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.4/js/bootstrap.min.js"></script>
+
+    </head>
+    <body>
+
+    </body>
+</html>
