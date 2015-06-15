9ae427d794bcf83d994d981e37cd1c1c3d27b938
unbolt <dan@unbo.lt>
2015-06-15 12:03:01 +0100

    Still battling namespaces

--- app/Http/Controllers/Auth/AuthenticateUser.php
+++ app/Http/Controllers/Auth/AuthenticateUser.php
@@ -4,7 +4,7 @@ namespace Shards\Http\Controllers\Auth;
 // AuthenticateUser.php
 use Illuminate\Contracts\Auth\Guard;
 use Laravel\Socialite\Contracts\Factory as Socialite;
-use Shards\Repositories\UserRepository; use Request;
+use Shards\Repositories\UserRepository; use Shards\Request;
 
 class AuthenticateUser {
 
