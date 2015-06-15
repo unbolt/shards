ce902b97270c9c16ed7b4a3cd747f75794bd99aa
unbolt <dan@unbo.lt>
2015-06-15 15:15:45 +0100

    Consistency... It's what's for dinner

--- resources/views/layouts/master.blade.php
+++ resources/views/layouts/master.blade.php
@@ -4,12 +4,12 @@
         <title>Shards - @yield('title')</title>
 
         <!-- include some styles -->
-        <link href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.4/css/bootstrap.min.css" rel="stylesheet">
+        <link href="//maxcdn.bootstrapcdn.com/bootstrap/3.3.4/css/bootstrap.min.css" rel="stylesheet">
         <link href="/css/shards.css" rel="stylesheet">
 
         <!-- include our scripts -->
         <script src="//code.jquery.com/jquery-1.11.3.min.js"></script>
-        <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.4/js/bootstrap.min.js"></script>
+        <script src="//maxcdn.bootstrapcdn.com/bootstrap/3.3.4/js/bootstrap.min.js"></script>
 
     </head>
     <body>
