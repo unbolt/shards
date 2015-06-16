47b8ae9b240be8ff4ce757164f891924a72862bf
unbolt <dan@unbo.lt>
2015-06-16 13:47:54 +0100

    Adding login form

--- app/Http/Controllers/Auth/AuthController.php
+++ app/Http/Controllers/Auth/AuthController.php
@@ -24,7 +24,7 @@ class AuthController extends Controller
 
     use AuthenticatesAndRegistersUsers;
 
-    protected $redirectTo = '/dashboard';
+    protected $redirectTo = 'dashboard';
 
     /**
      * Create a new authentication controller instance.
