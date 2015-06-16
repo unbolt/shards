bc63e079d44f69787cdf691ef9a98f176df52d49
unbolt <dan@unbo.lt>
2015-06-16 15:01:44 +0100

    Removing comment from gulpfile

--- gulpfile.js
+++ gulpfile.js
@@ -1,16 +1,5 @@
 var elixir = require('laravel-elixir');
 
-/*
- |--------------------------------------------------------------------------
- | Elixir Asset Management
- |--------------------------------------------------------------------------
- |
- | Elixir provides a clean, fluent API for defining some basic Gulp tasks
- | for your Laravel application. By default, we are compiling the Less
- | file for our application, as well as publishing vendor resources.
- |
- */
-
 elixir(function(mix) {
     mix.sass(['shards.scss'], "public/css/shards.css");
     mix.version("css/shards.css");
