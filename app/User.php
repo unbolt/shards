163a6deb3f806ee4c234133132b7b95ad520e57a
unbolt <dan@unbo.lt>
2015-12-11 09:26:16 +0000

    Linking users to characters

--- app/User.php
+++ app/User.php
@@ -32,4 +32,10 @@ class User extends Model implements AuthenticatableContract, CanResetPasswordCon
      * @var array
      */
     protected $hidden = ['password', 'remember_token'];
+
+
+    public function characters()
+    {
+        return $this->hasMany('Shards\Character');
+    }
 }
