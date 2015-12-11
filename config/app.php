600bc9ea44839ec596e5df94f27933cccebe1771
unbolt <dan@unbo.lt>
2015-12-11 09:25:42 +0000

    Updating dependencies

--- config/app.php
+++ config/app.php
@@ -137,6 +137,7 @@ return [
         Illuminate\Validation\ValidationServiceProvider::class,
         Illuminate\View\ViewServiceProvider::class,
         Kamaln7\Toastr\ToastrServiceProvider::class,
+        Collective\Html\HtmlServiceProvider::class,
 
         /*
          * Application Service Providers...
@@ -192,7 +193,9 @@ return [
         'URL'       => Illuminate\Support\Facades\URL::class,
         'Validator' => Illuminate\Support\Facades\Validator::class,
         'View'      => Illuminate\Support\Facades\View::class,
-        'Toastr'    => Kamaln7\Toastr\Facades\Toastr::class
+        'Toastr'    => Kamaln7\Toastr\Facades\Toastr::class,
+        'Form'      => Collective\Html\FormFacade::class,
+        'Html'      => Collective\Html\HtmlFacade::class
 
     ],
 
